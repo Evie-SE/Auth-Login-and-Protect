fe85a6e7ce5f5e1eea002fca8bfbc6fc4d160c8d
Evie-SE <jerianamaranguis08@gmail.com>
2026-08-14 22:10:02 +0800

    Stage 2: public route and unverified protected route

--- app.js
+++ app.js
@@ -56,6 +56,28 @@ app.post("/auth/login", async (req, res) => {
   });
 });
 
+app.get("/public/info", async (req, res) => {
+  return res
+    .status(200)
+    .json({ message: "Welcome stranger! This info is public." });
+});
+
+app.get("/protected/profile", async (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({error: "Access token required"});
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  if(!token) {
+    return res.status(401).json({error: 'Access token required'});
+  }
+
+  return res.status(200).json({message: "Token presented successfully"});
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
